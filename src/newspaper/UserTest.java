0565eb831526f8cd889ed99b30ce061a30ab3474
Gideon Isa <innocentgideon10@gmail.com>
2023-03-25 16:52:19 +0100

    added changes

--- src/newspaper/UserTest.java
+++ src/newspaper/UserTest.java
@@ -14,5 +14,18 @@ public class UserTest {
         ut.printUserType(user);
         ut.printUserType(staff);
         ut.printUserType(editor);
+
+        //editor.approveReview();
+        // compiler error. because it uses the reference variable editor which is of type User to access methods
+        // and not the object (new Editor) itself
+
+        editor.postAReview(); // it prints the postAReview in the Staff class because it is the immediate super class
+        // i.e. JVM is searching the inherited tree starting from the Editor class then Staff class then to User...
+
+        editor.saveWebLink();
+        // It starts search from the Editor class then up the inherited tree and get to the User class
+        // and prints the saveWebLink(). Which in turn calls the postAReview() then the process
+        // (search for the method) start again from the Editor class and up to the Staff class where it calls
+        // the PostAReview method of the Staff class
     }
 }
